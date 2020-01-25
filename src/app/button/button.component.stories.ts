3941c69459f0a0aed50b61cc970e0e4911700e53
Fitzgerald <afitzgerald@mbp-c02xx6n2jg5j.lan>
2020-01-24 23:39:08 -0600

    Added logo image

--- src/app/button/button.component.stories.ts
+++ src/app/button/button.component.stories.ts
@@ -1,28 +1,28 @@
 import { storiesOf, moduleMetadata, addDecorator, addParameters } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
-import { withCssResources } from '@storybook/addon-cssresources';
+// import { withCssResources } from '@storybook/addon-cssresources';
 // import { of } from 'rxjs';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { object } from '@storybook/addon-knobs';
 import { ButtonComponent } from '../button/button.component';
 
-addParameters({
-  cssresources: [
-    {
-        id: `red`,
-        code: `<style>body { background-color: red; }</style>`,
-        picked: true,
-    },
-    {
-        id: `yellow`,
-        code: `<style>body { background-color: yellow; }</style>`,
-        picked: false,
-    },
-  ],
-});
-
-addDecorator(withCssResources);
+// addParameters({
+//   cssresources: [
+//     {
+//         id: `red`,
+//         code: `<style>body { background-color: red; }</style>`,
+//         picked: true,
+//     },
+//     {
+//         id: `yellow`,
+//         code: `<style>body { background-color: yellow; }</style>`,
+//         picked: false,
+//     },
+//   ],
+// });
+//
+// addDecorator(withCssResources);
 
 storiesOf('Button', module)
     .addDecorator(
